2e49deb23d56a3fc3c2c631d5498c5ec53e10cf9
raphael29-code <ykim29@ssis.edu.vn>
2026-05-20 09:20:03 +0700

    Added magenta colored triangles to picture.py
    I added magenta colored triangles to the far left of the plane.

--- picture.py
+++ picture.py
@@ -28,26 +28,20 @@ def draw_picture(width, height):
     
     # code for BLUE triangles
     
-        
-
     
     # code for YELLOW triangles
-    
+   
+   
     # code for GREEN triangles
-    
-        
+ 
  
     # code for MAGENTA triangles
-    
-    
-    # code for CYAN triangles
-    sg.set_fill_color(colors[3]) # set fill color to blue
+    sg.set_fill_color(colors[4]) # set fill color to red
     
     for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
-        sg.fill_triangle(1*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT, 0*TRIANGLE_WIDTH, (i)*TRIANGLE_HEIGHT, 0*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
-        
+        sg.fill_triangle(0*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 1*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 1*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
     
-    
+    # code for CYAN triangles
 
 if __name__ == "__main__":
     # Launch the wrapper; only edit starting dimensions of canvas if you would like to
